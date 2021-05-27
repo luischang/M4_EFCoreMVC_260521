5ce55be03ea3608931cd9518322a35cda1af8783
Luis Chang <luis.chang@outlook.com>
2021-05-26 23:03:14 -0500

    Agregar archivos de proyecto.

---
+++ M4_EFCoreMVC.CodeFirst/Models/EurocopaDBContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace M4_EFCoreMVC.CodeFirst.Models
+{
+    public class EurocopaDBContext: DbContext
+    {
+
+        public EurocopaDBContext(DbContextOptions<EurocopaDBContext> options): base(options)
+        {
+
+        }
+
+        public virtual DbSet<Player> Player { get; set; }
+
+
+    }
+}
---
+++ M4_EFCoreMVC.CodeFirst/Models/Player.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace M4_EFCoreMVC.CodeFirst.Models
+{
+    [Table("Player")]
+    public class Player
+    {
+        [Key]
+        public int Id { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        [Display(Name ="Futbolista")]
+        public string FullName { get; set; }
+        [Column(TypeName = "int")]
+        [Display(Name = "N° Camiseta")]
+        [Range(1, 30, ErrorMessage = "The field {0} must be greater than {1}.")]
+        public int Dorsal { get; set; }
+        [Column(TypeName = "Date")]
+        [Display(Name = "Fec.Nacimiento")]
+        [DataType(DataType.Date)]
+        public DateTime DateOfBirth { get; set; }
+        
+    }
+}
